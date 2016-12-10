f30cff096cd71f715d2002553ee6b82363bccc05
James Lu <jamtlu@gmail.com>
2016-12-10 13:04:58 -0500

    Extremely basic super incomplete framework

---
+++ logic.py
@@ -0,0 +1,34 @@
+from abc import ABCMeta
+
+
+class Order(metaclass=ABCMeta):
+    """All orders inherit from this order."""
+
+    @staticmethod
+    def execute(bot):
+        raise NotImplementedError()
+
+
+class Bot(object):
+    """
+    Attribute API:
+    bot_team - access team of bot
+    orders - acc
+    """
+
+    def __init__(self, initial_order: Order):
+        self.orders = initial_order
+        ...
+
+
+class GameState(object):
+    """
+    Put helper functions to not pollute the namespace.
+    Because globals are bad.
+    Directly modify properties.
+    """
+
+    bots = []
+
+    def __init__(self):
+        pass
---
+++ main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+from logic import Order, Bot, GameState
+
+if __name__ == '__main__':
+    # code when run in main process goes here.
+    game = GameState()
+
